0b9e53c9137b57141d795fe6bbda7ebf481901eb
venkatesha <venkatesha@flexibleir.com>
2019-03-24 13:57:16 +0530

    client implementation. need to fix the build still

--- connectors/pciconnector.go
+++ connectors/pciconnector.go
@@ -1,11 +1,36 @@
 package connectors
 
-import "github.com/flexibleir/orchestrator/data/logindetails"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/flexibleir/orchestrator/data/logindetails"
+	apiclient "github.com/flexibleir/orchestrator/gen/client/compliance/client"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
+)
 
 // PcIConnector - PcI connector for communicating with PcI container
 type PcIConnector struct{}
 
 // Run - Implementation of interface method baseconnector
 func (pci *PcIConnector) Run(*logindetails.LoginDetails) string {
+
+	// create the transport
+	transport := httptransport.New(os.Getenv("TODOLIST_HOST"), "", nil)
+
+	// create the API client, with the transport
+	client := apiclient.New(transport, strfmt.Default)
+
+	// to override the host for the default client
+	// apiclient.Default.SetTransport(transport)
+
+	// make the request to get all items
+	resp, err := client.Compliance.Create()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%#v\n", resp.Payload)
 	return ""
 }
